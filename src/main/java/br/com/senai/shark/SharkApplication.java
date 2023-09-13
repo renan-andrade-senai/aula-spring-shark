31e3f32892952e5c01779a2c2624cd35285aecee
renan-andrade-senai <renan.andrade@edu.sc.senai.br>
2023-09-13 17:39:40 -0300

    ajuste cors

--- src/main/java/br/com/senai/shark/SharkApplication.java
+++ src/main/java/br/com/senai/shark/SharkApplication.java
@@ -2,12 +2,25 @@ package br.com.senai.shark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SharkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SharkApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
